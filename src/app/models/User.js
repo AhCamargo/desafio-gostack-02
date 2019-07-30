17dfded1b9e7abae98ea276ebbee3f5f9b7fc027
AhCamargo <andre.camargo500@gmail.com>
2019-07-30 22:27:27 +0000

    desafio 2

---
+++ src/app/models/User.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-param-reassign */
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // só existirá aqui no código e nunca na base.
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN, // Seria tipo um usuário Admin(Coringa);
+      },
+      {
+        sequelize,
+      },
+    );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'ahcamargo',
+  password: 'alc123',
+  database: 'events',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
